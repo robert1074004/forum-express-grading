7e7fe12f7e2fb41574dd55a3c83d3bc4a861a475
robert1074004 <abc50113@yahoo.com.tw>
2022-10-29 11:20:18 +0800

    feat: add deleteRestaurant function in adminServices

--- services/admin-services.js
+++ services/admin-services.js
@@ -9,6 +9,19 @@ const adminServices = {
     return Restaurant.findAndCountAll({ limit, offset, raw: true, nest: true, include: [Category] })
       .then(restaurants => { return cb(null, { restaurants: restaurants.rows, pagination: getPagination(limit, page, restaurants.count) }) })
       .catch(err => cb(err))
+  },
+  deleteRestaurant: (req, cb) => {
+    return Restaurant.findByPk(req.params.id)
+      .then(restaurant => {
+        if (!restaurant) {
+          const err = new Error("Restaurant didn't exist !")
+          err.status = 404
+          throw err
+        }
+        return restaurant.destroy()
+      })
+      .then(deletedRestaurant => cb(null, { restaurant: deletedRestaurant }))
+      .catch(err => cb(err))
   }
 }
 module.exports = adminServices
