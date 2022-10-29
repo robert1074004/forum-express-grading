c76e20cc56b65a008e542bd785b8ec15c5772c0c
robert1074004 <abc50113@yahoo.com.tw>
2022-10-30 00:14:52 +0800

    feat: add create restaurant api

--- services/admin-services.js
+++ services/admin-services.js
@@ -1,5 +1,6 @@
 const { Restaurant, Category } = require('../models')
 const { getOffset, getPagination } = require('../helpers/pagination-helper')
+const { imgurFileHandler } = require('../helpers/file-helpers')
 const adminServices = {
   getRestaurants: (req, cb) => {
     const DEFAULT_LIMIT = 5
@@ -21,6 +22,17 @@ const adminServices = {
         return restaurant.destroy()
       })
       .then(deletedRestaurant => cb(null, { restaurant: deletedRestaurant }))
+      .catch(err => cb(err))
+  },
+  postRestaurant: (req, cb) => {
+    const { name, tel, address, openingHours, description, categoryId } = req.body
+    if (!name) throw new Error('Restaurant name is required')
+    const { file } = req
+    return imgurFileHandler(file)
+      .then(filePath => {
+        return Restaurant.create({ name, tel, address, openingHours, description, image: filePath || null, categoryId })
+      })
+      .then(newRestaurant => cb(null, { restaurant: newRestaurant }))
       .catch(err => cb(err))
   }
 }
